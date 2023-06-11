d38a65a117791c33adae73be028ca2f35cafc9c7
mooshra <31731931+mooshra@users.noreply.github.com>
2023-06-11 13:24:10 +0300

    delete all changes

--- Assets/Scripts/FirstPersonController.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:50e806178266a5cbc37f549bdb0b41733249296ccb97b7e22c66946a6c4b1f21
-size 8601
--- Assets/TutorialInfo/Scripts/Editor/ReadmeEditor.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:25a8a4ccffc0a2534cf7c42f6e5aff4112cbf55a680485f811d430585773bbd7
-size 4083
